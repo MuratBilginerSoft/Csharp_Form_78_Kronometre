84a9f1fd97a9457e52c04a1e0ab312f11fe0fca0
Murat Bilginer <muratbilginer09@gmail.com>
2020-03-25 23:18:01 +0300

    Initial commit

---
+++ KRONOMETRE/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace KRONOMETRE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(32, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ne Kadar Zaman?";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(118, 32);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(157, 21);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(14, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(350, 98);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Başlangıç";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(222, 64);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(52, 20);
+            this.radioButton3.TabIndex = 11;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Saat";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(118, 64);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(39, 20);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Sn";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(169, 64);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(39, 20);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Dk";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(282, 32);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(61, 28);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Başlat";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(14, 121);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox2.Size = new System.Drawing.Size(350, 80);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Kronometre";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(282, 33);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(60, 27);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Durdur";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(378, 218);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "KRONOMETRE";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+    }
+}
+
---
+++ KRONOMETRE/Form1.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace KRONOMETRE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region tanımlamalar
+
+
+        int a1=0, b1=0, c1=0, d1=0;
+
+        int dx = 0;
+
+        #endregion
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+         radioButton1.Enabled = false;
+         radioButton2.Enabled = false;
+         radioButton3.Enabled = false;
+            button1.Enabled = false;
+            button2.Enabled = false;
+
+            a1 = 0;
+            b1 = 0;
+            c1 = 0;
+            d1 = 0;
+            label1.Text = "0" + a1.ToString() + ":0" + b1.ToString() + ":0" + c1.ToString() + ":0" + d1.ToString();
+            
+           
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            if (radioButton1.Checked == true)
+            {
+                if (d1 == 0)
+                {
+                    if (c1 != 0)
+                    {
+                        c1--;
+                        d1 = 99;
+                        label1.Text = "0" + a1.ToString() + ":0" + b1.ToString() + ":" + c1.ToString() + ":" + d1.ToString();
+
+
+                    }
+                    else
+                    {
+
+                        label1.Text = "0" + a1.ToString() + ":0" + b1.ToString() + ":0" + c1.ToString() + ":0" + d1.ToString();
+                        timer1.Stop();
+                        MessageBox.Show("Time İs Up!!!");
+                        radioButton1.Enabled = true;
+                        radioButton2.Enabled = true;
+                        radioButton3.Enabled = true;
+                       
+                        
+                    
+                    }
+
+                    
+                
+                
+                }
+                else
+                {
+                    d1--;
+                    label1.Text = "0" + a1.ToString() + ":0" + b1.ToString() + ":" + c1.ToString() + ":" + d1.ToString();
+
+                }
+              
+            
+            
+            }
+
+            else if (radioButton2.Checked==true)
+            {
+                if (d1 == 0)
+                {
+                    if (c1 == 0)
+                    {
+                        if (b1 != 0)
+                        {
+                            b1--;
+                            c1 = 59;
+                            d1 = 99;
+                            label1.Text = "0" + a1.ToString() + ":" + b1.ToString() + ":" + c1.ToString() + ":" + d1.ToString();
+                        }
+
+                        else if(b1==0)
+                        {
+                            label1.Text = "0" + a1.ToString() + ":0" + b1.ToString() + ":0" + c1.ToString() + ":0" + d1.ToString();
+                            timer1.Stop();
+                            MessageBox.Show("Time İs Up!!!");
+                            radioButton1.Enabled = true;
+                            radioButton2.Enabled = true;
+                            radioButton3.Enabled = true;
+                        }
+
+                    }
+
+                    else if (c1 != 0)
+                    {
+                        c1--;
+                        d1 = 99;
+                        label1.Text = "0" + a1.ToString() + ":" + b1.ToString() + ":" + c1.ToString() + ":" + d1.ToString();
+                    
+                    }
+
+
+                } 
+
+                else
+                {
+
+                        d1--;
+                        label1.Text = "0" + a1.ToString() + ":" + b1.ToString() + ":" + c1.ToString() + ":" + d1.ToString();
+
+                
+                }
+            
+            
+            
+            
+            }
+
+            else if (radioButton3.Checked == true)
+            {
+                if (d1 == 0)
+                {
+                    if (c1 == 0)
+                    {
+                        if (b1 == 0)
+                        {
+                            if (a1 != 0)
+                            {
+                                a1--;
+                                b1 = 59;
+                                c1 = 59;
+                                d1 = 99;
+                                label1.Text = "0" + a1.ToString() + ":" + b1.ToString() + ":" + c1.ToString() + ":" + d1.ToString();
+
+                            }
+
+                            else if (a1 == 0)
+                            {
+                                label1.Text = "0" + a1.ToString() + ":0" + b1.ToString() + ":0" + c1.ToString() + ":0" + d1.ToString();
+                                timer1.Stop();
+                                MessageBox.Show("Time İs Up!!!");
+                                radioButton1.Enabled = true;
+                                radioButton2.Enabled = true;
+                                radioButton3.Enabled = true;
+                            }
+
+                        }
+
+                        else if (b1 != 0)
+                        {
+                            b1--;
+                            c1 = 59;
+                            d1 = 99;
+                            label1.Text = a1.ToString() + ":" + b1.ToString() + ":" + c1.ToString() + ":" + d1.ToString();
+
+
+                        }
+
+                    }
+
+                    else if (c1 != 0)
+                    {
+                        c1--;
+                        d1 = 99;
+                        label1.Text = a1.ToString() + ":" + b1.ToString() + ":" + c1.ToString() + ":" + d1.ToString();
+
+
+                    }
+
+
+
+                }
+
+                else
+                {
+                    d1--;
+                    label1.Text = a1.ToString() + ":" + b1.ToString() + ":" + c1.ToString() + ":" + d1.ToString();
+                
+                }
+            
+            
+            }
+
+
+           
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            radioButton1.Enabled = false;
+            radioButton2.Enabled = false;
+            radioButton3.Enabled = false;
+
+            if (radioButton1.Checked == true)
+            {
+                a1 = 0; b1 = 0; c1 = int.Parse(textBox1.Text); d1 = 0;
+                label1.Text = "0" + a1.ToString() + ":0" + b1.ToString() + ":0" + c1.ToString() + ":0"+d1.ToString();
+                timer1.Start();
+              
+            
+            }
+
+            else if (radioButton2.Checked== true)
+            {
+                a1 = 0; b1 = int.Parse(textBox1.Text); c1 = 0; d1 = 0;
+                label1.Text = a1.ToString() + ":" + b1.ToString() + ":0" + c1.ToString() + ":0" + d1.ToString();
+                timer1.Start();
+            
+            
+            }
+
+            else if (radioButton3.Checked == true)
+            {
+                a1 = int.Parse(textBox1.Text); b1 = 0; c1 = 0; d1 = 0;
+                label1.Text = a1.ToString() + ":" + b1.ToString() + ":0" + c1.ToString() + ":0" + d1.ToString();
+                timer1.Start();
+            
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                radioButton1.Enabled = false;
+                radioButton2.Enabled = false;
+                radioButton3.Enabled = false;
+                button1.Enabled = false;
+                button2.Enabled = false;
+            }
+
+            else
+            {
+                radioButton1.Enabled = true;
+                radioButton2.Enabled = true;
+                radioButton3.Enabled = true;
+               
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                if ((int)e.KeyChar == 48)
+                    e.Handled = true;
+                else
+                    e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+
+            }
+
+            else
+                e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (dx % 2 == 0)
+            {
+                timer1.Stop();
+                button2.Text = "Devam Et";
+                dx++;
+
+            }
+
+            else
+            {
+                timer1.Start();
+                button2.Text = "Durdur";
+                dx++;
+            
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+        }
+    }
+}
